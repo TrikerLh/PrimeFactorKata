29efddc915754c79e281995d31c8e7824770ddf5
Raúl Corchero <corchero100@gmail.com>
2024-09-14 22:45:42 +0100

    refactro: generalize algorithm

--- PrimeFactorKata/PrimeFactorKata/PrimeFactor.cs
+++ PrimeFactorKata/PrimeFactorKata/PrimeFactor.cs
@@ -5,21 +5,20 @@ public class PrimeFactor
 	public List<int> Calculate(int numberToDecompose)
 	{
 		var factors = new List<int>();
+		var divisor = 2;
+
+		while (divisor < numberToDecompose) {
+			if (numberToDecompose % divisor == 0)
+			{
+				factors.Add(divisor);
+				numberToDecompose = (int)Math.Floor((decimal)numberToDecompose / divisor);
+			}
+
+			divisor++;
+		}
+
 		if (numberToDecompose > 1)
 		{
-			while (numberToDecompose % 2 == 0 && numberToDecompose > 2)
-			{
-				factors.Add(2);
-				numberToDecompose = (int)Math.Floor((decimal)numberToDecompose / 2);
-			}
-
-			if (numberToDecompose == 9)
-			{
-				factors.Add(3);
-				factors.Add(3);
-				return factors;
-			}
-
 			factors.Add(numberToDecompose);
 		}
 		return factors;
